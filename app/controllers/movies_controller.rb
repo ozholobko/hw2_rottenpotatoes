f518a627ec73cb1190ef15ca5cf0bddcbe5f9331
ozholobko <o.zholobko@gmail.com>
2012-03-12 15:05:16 -0700

    Added rating filter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,17 @@ class MoviesController < ApplicationController
   end
 
   def index
+    get_all_ratings
+    if params.has_key?(:ratings)
+       params[:ratings].keys.map{|x| @checked_boxes[x] = true}
+       @movies = Movie.where(:rating => params[:ratings].keys)
+      end
     if params.has_key? :sort_by
-      @movies = Movie.order params[:sort_by]
-      # This is pretty ugly:
+      if @movies.nil? || @movies.empty?
+        @movies = Movie.order params[:sort_by]
+      else
+        @movies = @movies.order params[:sort_by]
+      end
       if params[:sort_by] == 'title'
         @hilite_title_header = 'hilite'
       elsif params[:sort_by] == 'release_date'
@@ -47,5 +55,15 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  def get_all_ratings
+    @all_ratings =[]
+    Movie.select(:rating).map{|movie| @all_ratings.push(movie.rating)}
+    @all_ratings = @all_ratings.uniq.sort
+    if @checked_boxes.nil?
+      @checked_boxes = {}
+    end
+    @all_ratings.map{|x| @checked_boxes[x] = false}
+  end 
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+
+  def self.all_ratings
+    Movie.find_by_sql "SELECT DISTINCT rating FROM movies"
+  end
+  
 end
